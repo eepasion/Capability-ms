40d617f525dcdc119e41ac67f86e055b146184f0
eepasion <elias.pasion@pragma.com.co>
2025-02-24 09:53:06 -0500

    first commit

---
+++ src/main/java/com/pragma/capability_mf/CapabilityMfApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.capability_mf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapabilityMfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapabilityMfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/capability_mf/CapabilityMfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.capability_mf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapabilityMfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
